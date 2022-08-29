24f6bc9cb01daf91c8dc9eac1c5af8e36c302631
alwil <alexandra.wilczynska@gmail.com>
2022-08-29 17:07:16 +0200

    upade conditional UI

--- app/app.py
+++ app/app.py
@@ -1,10 +1,22 @@
-from shiny import App, render, ui
+from shiny import App, render, ui, reactive
 #from src.api_uploader import * 
 
 choices = {"a": "Choice A", "b": "Choice B"}
 action_choices = {'a': 'Upload files to 4TU repository', 'b': 'Browse and retrieve files from 4TU repository'}
 col_choices = {'grout': 'Grout Collection', 'xxx': 'xxx', 'yyy': 'yyy', 'zzz': 'zzz'}
 yn_choices = {'y':'Yes','n':'No'}
+
+valid_users = {'awilczynski', 'acryan', 'mvankoningsveld', 'fedorbaart'}
+check_show_ui = f"input.checkID && input.netid in  {valid_users}"
+
+def verify_id(user_input, valid_users ):
+    if user_input ==None: 
+        return(False) 
+    else:    
+        user_input_clean = user_input.lower()
+        return(user_input_clean in valid_users)
+
+
 
 app_ui = ui.page_fluid(
     ui.panel_title("GEF files handler"),
@@ -17,26 +29,91 @@ app_ui = ui.page_fluid(
 
     ui.layout_sidebar(
         ui.panel_sidebar(
-
             ui.input_text("netid", "Provide netID:", placeholder="Enter netID"), # isolate
-            ui.input_select("collection", "Which collection are you interested in?", col_choices),
-            ui.input_radio_buttons("sandbox", "Do you want to continue in the Sandbox environment?", yn_choices),
-            ui.input_password ("api_token", "Provide API token:", placeholder="Enter token")# isolate
-            #ui.input_select("action", "What do you want to do next?", action_choices),
+            ui.input_action_button("checkID", "Start"),
+            ui.output_ui("ui_sidebar"),
         ),
-    ui.panel_main(
-        ui.output_text_verbatim("txt")
-        )
+        ui.panel_main(
+        ui.navset_tab(
+
+            ui.nav("Welcome",
+            ui.h4("How to use this app?"),
+            ui.p(
+                """
+                When we train Machine Learning models like linear regressions, logistic
+                regressions, or neural networks, we do so by defining a loss function
+                and minimizing that loss function. A loss function is a metric for
+                measuring how your model is performing where lower is better. For
+                example, Mean Squared Error is a loss function that measures the squared
+                distance (on average) between a model's guesses and the true values."""
+            ),
+            
+            ),
+            
+            ui.nav("Upload",
+             ui.output_ui("ui_filepath") 
+             ),
+
+            ui.nav("Download",
+             "tab download content"
+             )
+
+            )    
+    )
     )
 
 )
 
 
 def server(input, output, session):
+    @reactive.Effect
+    @reactive.event(input.checkID)
+    def netIDnotification():
+        if(verify_id( input.netid(),valid_users )):
+            ui.notification_show(f"Session open for netID: {input.netid()}")
+        else:    
+            ui.notification_show("netID unknown", type="error")
+
+    @reactive.Effect
+    @reactive.event(input.checkID)
+    def netIDerror():
+        m = ui.modal(
+        "Provide a valid netID",
+        title="Unknown netID!",
+        easy_close=True,
+        footer=None
+        )
+
+        if(not verify_id( input.netid(),valid_users )):
+            ui.modal_show(m)        
+
+    @reactive.Effect
+    @reactive.event(input.sidebar_complete)
+    def SidebarCompletenotification():
+        if( not (input.api_token() and input.sandbox() and input.collection()) ):    
+            ui.notification_show("Selection incomplete", type="error")        
+     
     @output
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+    @render.ui
+    def ui_sidebar():
+        input.checkID()
+        with reactive.isolate():
+            verified = verify_id( input.netid(),valid_users )
+            if verified:
+                return ui.TagList(
+                    ui.input_select("collection", "Which collection are you interested in?", col_choices, selected = None),
+                    ui.input_radio_buttons("sandbox", "Do you want to continue in the Sandbox environment?", yn_choices, selected = None),
+                    ui.input_password ("api_token", "Provide API token:", placeholder="Enter token"),
+                    ui.input_action_button("sidebar_complete", "Run")
+                    )
+    
+    @output
+    @render.ui
+    def ui_filepath():
+        input.sidebar_complete()
+        with reactive.isolate():
+            if input.api_token() and input.sandbox() and input.collection():
+                return( ui.input_file("file_upload", "Choose file(s) to upload:", multiple=True) )
 
 
 app = App(app_ui, server)
